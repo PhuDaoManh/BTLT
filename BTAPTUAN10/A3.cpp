2d0893109d277bf0054bd6e37f76ac2ea8fe3ffe
PhuDaoManh <158453994+PhuDaoManh@users.noreply.github.com>
2024-04-23 22:30:20 +0700

    bt

--- BTAPTUAN10/A3.cpp
+++ BTAPTUAN10/A3.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
+//khong hien duoc cerr thu 3
 char* a = new char[10];
 char* c = a + 3;
 for (int i = 0; i < 9; i++) a[i] = 'a';
 a[9] = '\0';
 cerr <<"a: " << "-" << a << "-" << endl;
 cerr <<"c: " << "-" << c << "-" << endl;
-delete c;
+delete c;//khong xoa dung con tro duoc cap phat dong
 cerr << "a after deleting c:" << "-" << a << "-" << endl;
 
 }
--- BTAPTUAN10/A4.cpp
+++ BTAPTUAN10/A4.cpp
@@ -3,6 +3,5 @@ using namespace std;
 int main(){
    int a=4;
     int *p=&a;
-   delete p;//khi chay thi no khong hien loi nhung co ve nhu ta da truy cap vao vung nho khong hop le
-
+   delete p;// loi khi ta tro den vung nho da duoc giai phong( bien dia phuong duoc giai phong)
 }
