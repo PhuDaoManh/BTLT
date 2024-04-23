e6d4591acc3736ba1dd84819f0ef5aed0cd81d82
PhuDaoManh <158453994+PhuDaoManh@users.noreply.github.com>
2024-04-23 22:36:53 +0700

    A3.cpp

--- BTAPTUAN10/A3.cpp
+++ BTAPTUAN10/A3.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main(){
+//khong hien duoc cerr thu 3
 char* a = new char[10];
 char* c = a + 3;
 for (int i = 0; i < 9; i++) a[i] = 'a';
 a[9] = '\0';
 cerr <<"a: " << "-" << a << "-" << endl;
 cerr <<"c: " << "-" << c << "-" << endl;
-delete c;
+delete c;//không xóa đúng con trỏ được cấp phát động
 cerr << "a after deleting c:" << "-" << a << "-" << endl;
 
 }
